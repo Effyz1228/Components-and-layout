8c1abe67a5306bc0af2b1a37ad7ceb82438167b4
Effy Zhang <effyz1228@gmail.com>
2022-05-13 11:18:57 -0700

    finish implementing accordion component with JS

---
+++ app.js
@@ -0,0 +1,9 @@
+const btns= document.querySelectorAll('.icon');
+
+for(let btn of btns){
+    const item =btn.parentElement;
+
+    btn.addEventListener('click',()=>{
+        item.classList.toggle('open');
+        });
+    }
